d22c13d3d1da7c51c9481a288ab622c27cdaa2b5
Momelezi <kmchunu029@gmail.com>
2022-05-02 23:12:39 +0200

    Cleaned up code and made it testable

--- Classifier_Adam.py
+++ Classifier_Adam.py
@@ -5,30 +5,29 @@ import torch.utils.data as data
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose,transforms
 from PIL import Image
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-
-l_function = nn.CrossEntropyLoss()
 accuracy_counter =[]
 loss_counter = []
-image =  Image.open('img_2.jpg')
-transformImage = transforms.Compose([transforms.ToTensor()])
-image_tensor = transformImage(image)
+
 class network(nn.Module):
     def __init__(self):
         super(network,self).__init__()
         self.net = nn.Sequential(
             nn.Linear(784,512),
             nn.ReLU(),
-            nn.Linear(512,10)
-            nn.LogSoftmax(dim=1)
+            nn.Linear(512,10),
+            nn.Softmax(dim=1)
         )
     def forward(self,input):
         input = torch.flatten(input,1)
         output = self.net(input)
         return output
-model = network()
-print(model)
-opti = optim.Adam(model.parameters())
+
+
 def accuracy(logits, target):
     predicted = logits.argmax(dim=1)
     return (predicted == target).type(torch.float).mean()
@@ -36,7 +35,6 @@ def flatten(data):
     return data.reshape(-1)
 
 def model_train(data,model,optim,loss_function):
-    print("Starting training------------------------")
     size=  len(data.dataset)
     model.train()
     model_loss=[]
@@ -45,9 +43,11 @@ def model_train(data,model,optim,loss_function):
         predicted_value = model(train)
         loss_value = loss_function(predicted_value,expected)
         model_loss.append(loss_value.detach())
-        opti.zero_grad()
+        optim.zero_grad()
         loss_value.backward()
-        opti.step()
+        optim.step()
+    if current_batch%100==0:
+        current = current_batch*len(train)
     
     loss_counter.append(torch.tensor(model_loss).mean())
 def model_test(data, model):
@@ -58,16 +58,46 @@ def model_test(data, model):
         logits = model(x)
         acc = accuracy(logits, target)
         accuracy_counter.append(acc)
+def obtain_trained():
+    model = network()
+    print(model)
+    opti = optim.Adam(model.parameters())
+    l_function = nn.CrossEntropyLoss()
+    transform =  transforms.Compose([transforms.ToTensor(),flatten])
+    mnist_train_data =  datasets.MNIST(root="./", train = True,download=False,transform=transform)
+    mnist_test_data =  datasets.MNIST(root="./",train = False,download=False,transform=transform) 
+    train_data = data.DataLoader(mnist_train_data,batch_size=64,shuffle=True)
+    test_data = data.DataLoader(mnist_test_data,batch_size=64,shuffle=True)
+    for i in range(10):
+        model_train(train_data,model,opti,l_function)
+        model_test(test_data,model)
+    torch.save(model,'models/Classifier_Adam.pt')
+def plot(accuracy,loss):
+    plt.plot(accuracy,label='Validation Accuraccy')
+    plt.plot(loss,label='Training Loss')
+    plt.legend(frameon=False)
+    plt.savefig('plot.png')
+    plt.show()
+def predict_image(image):
+    saved_model = torch.load('models/Classifier_Adam.pt')
+    saved_model.eval()
+    image_in =  Image.open(image)
+    transformImage = transforms.Compose([transforms.ToTensor()])
+    image_tensor = transformImage(image_in)
+    output = saved_model(image_tensor)
+    index =  output.data.cpu().numpy().argmax()
+    return index
 
-transform =  transforms.Compose([transforms.ToTensor(),flatten])
-mnist_train_data =  datasets.MNIST(root="./", train = True,download=False,transform=transform)
-mnist_test_data =  datasets.MNIST(root="./",train = False,download=False,transform=transform) 
-train_data = data.DataLoader(mnist_train_data,batch_size=64,shuffle=True)
-test_data = data.DataLoader(mnist_test_data,batch_size=64,shuffle=True)
-image =  data.DataLoader(image_tensor)
-# for i in range(20):
-#     model_train(train_data,model,opti,l_function)
-#     model_test(test_data,model)
-print(model(image_tensor))
-# print(accuracy_counter)
-# print(loss_counter)
+# print(predict_image("MNIST_JPGS/trainingSample/img_3.jpg"))
+def main():
+    obtain_trained()
+    print(predict_image("MNIST_JPGS/trainingSample/img_3.jpg"))
+    if os.path.exists("model/Classifier_Adam.pt"):
+        #Get image path and call predict image fn
+        pass
+    else:
+        pass
+        # get imag
+
+if __name__ == "__main__":
+    main()
